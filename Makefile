66ecce061f297f14d8939e1e4555b5c2ca05f070
paulo.barbosa <paulo.barbosa@farfetch.com>
2021-12-28 14:26:49 +0000

    feat: Add optimized code

--- Makefile
+++ Makefile
@@ -4,6 +4,11 @@ run_benchmark_option_1:
 # only on unix
 	time python test_benchmark.py
 
+### another benchmark options is to -> run timeit in IPython
+## poetry run ipython
+## from main_optimized import benchmark
+## %timeit benchmark()
+
 run_benchmark_option_2:
 	python -m timeit -s 'from test_benchmark import benchmark' 'benchmark()'
 
---
+++ test_benchmark_optimized.py
@@ -0,0 +1,22 @@
+from random import uniform
+from main_optimized import Particle, ParticleSimulator
+
+"""
+A test ensures the correctness of our functionality but 
+    gives little information about its running time.
+"""
+def benchmark():
+    particles = [
+        # What is the uniform method?
+        # gives you a random floating point number in the range [a, b],
+        #   (where rounding may end up giving you b).
+        # ref: https://stackoverflow.com/questions/30030659/in-python-what-is-the-difference-between-random-uniform-and-random-random/53817495
+        Particle(uniform(-1.0, 1.0), uniform(-1.0, 1.0), uniform(-1.0, 1.0)) for _ in range(1000)
+    ]
+
+    simulator = ParticleSimulator(particles)
+    simulator.evolve(0.1)
+
+
+if __name__ == '__main__':
+    benchmark()
